1b5b82fa09a90b6cfc51ecb288fa4741a94e3f16
Varun Chinthakunta <115773053+VarunC05@users.noreply.github.com>
2025-06-15 23:17:23 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Main{
+
+    public static void main(String[] args){
+
+        System.out.println("This is Java Program");
+        System.out.println("Output Java Program");
+        System.out.println("Time now: " + new Date());
+        System.out.println("Hello");
+    }
+} 
