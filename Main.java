abd36979a462ae0898cc5c576c48fec3944e068a
Varun Chinthakunta <115773053+VarunC05@users.noreply.github.com>
2025-06-15 23:38:32 +0530

    added one more line

--- Main.java
+++ Main.java
@@ -8,5 +8,6 @@ public class Main{
         System.out.println("Output Java Program");
         System.out.println("Time now: " + new Date());
         System.out.println("Hello World");
+        System.out.println("Checking Auto Build and Deploy for the first time");
     }
 } 
